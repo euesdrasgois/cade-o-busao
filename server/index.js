9a86b2feef5f08afef7efc7a7e101bbbb5344302
Esdras Gois <gsesdras@gmail.com>
2020-07-22 20:51:37 -0300

    removing app

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const sockets = require("socket.io").listen(server);
+const port = 3000;
+
+const buses = {
+    bus1: {
+        coords: {
+            latitude: -10.9993773,
+            longitude: -37.3131371,
+        },
+    },
+};
+
+sockets.on("connection", (socket) => {
+    console.log("Connected");
+    socket.emit("buses", buses);
+});
+
+sockets.on("disconnect", (socket) => {
+    console.log("> User Disconnected");
+});
+
+server.listen(port, () => console.log("Server listening on port: " + port));
